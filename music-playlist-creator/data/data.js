2c4365a35ec4a605bc82512cddfe316080b9df42
Mahesh Andres Bachu <maheshbachu@hotmail.com>
2025-06-04 16:57:27 -0700

    Many updates, including dynamic data and basic css

---
+++ music-playlist-creator/data/data.js
@@ -0,0 +1,34 @@
+let playlists = [
+	{
+		"playlistID": 1,
+		"playlist_name": "Chill",
+		"playlist_author": "Joe",
+		"playlistArt": "assets\img\playlist.png",
+		"songs": ["Cool Breeze", "ABCs"],
+		"likes": 12,
+	},
+	{
+		"playlistID": 3,
+		"playlist_name": "Chill1",
+		"playlist_author": "Joe",
+		"playlistArt": "assets\img\playlist.png",
+		"likes": 13,
+		"songs": ["Cool Breeze", "ABCs"]
+	},
+	{
+		"playlistID": 1,
+		"playlist_name": "Chill2",
+		"playlist_author": "Joe",
+		"playlistArt": "assets\img\playlist.png",
+		"likes": 14,
+		"songs": ["Cool Breeze", "ABCs"]
+	},
+	{
+		"playlistID": 1,
+		"playlist_name": "Chill3",
+		"playlist_author": "Joes",
+		"playlistArt": "assets\img\playlist.png",
+		"likes": 15,
+		"songs": ["Cool Breeze", "ABCs"]
+	},
+]
--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,49 @@
 
+// JavaScript for Opening and Closing the Modal
+const modal = document.getElementById("playListModal");
+const span = document.getElementsByClassName("close")[0];
+
+function openModal(festival) {
+   
+   modal.style.display = "block";
+}
+
+span.onclick = function() {
+   modal.style.display = "none";
+}
+window.onclick = function(event) {
+   if (event.target == modal) {
+      modal.style.display = "none";
+   }
+}
+
+//Code for Playlist rendering
+
+const createPlaylist = (playlist) => {
+    console.log(playlist);
+    const playlistElement = document.createElement('div');
+    playlistElement.className = 'playList';
+    playlistElement.onclick = "openModal()"
+    playlistElement.innerHTML= `
+    <img width="100px" src="playlist.png">
+    <h4>${playlists[playlist].playlist_name}</h3>
+    <h6>${playlists[playlist].playlist_author}</h6>
+    <div class="likes">
+        <h6>&#x2665</h6>
+        <h6>${playlists[playlist].likes}</h6>
+    </div>`
+    ;
+    return playlistElement;
+}
+
+const loadPlaylists = () =>{
+    const container = document.querySelector('#playListGrid');
+    for(const playlist in playlists){
+        const el = createPlaylist(playlist);
+        container.appendChild(el);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadPlaylists();
+});
