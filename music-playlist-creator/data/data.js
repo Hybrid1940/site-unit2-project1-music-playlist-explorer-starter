a76cb42fe05a4dc93b1b492a7d34ae55c9237ec4
Mahesh Andres Bachu <maheshbachu@hotmail.com>
2025-06-04 22:34:58 -0700

    Up to milestone 4!!!

--- music-playlist-creator/data/data.js
+++ music-playlist-creator/data/data.js
@@ -3,23 +3,25 @@ let playlists = [
 		"playlistID": 1,
 		"playlist_name": "Chill",
 		"playlist_author": "Joe",
-		"playlistArt": "assets\img\playlist.png",
-		"songs": ["Cool Breeze", "ABCs"],
+		"playlistArt": "assets/img/945cb857460591da1840c1097a7f8917.png",
+		"songs": [
+			["assets/img/945cb857460591da1840c1097a7f8917.png", "Cool Breeze", "John Macentire", "sacared", "4:52"], 
+			["assets/img/945cb857460591da1840c1097a7f8917.png", "Cool Breeze", "John Macentire", "sacared", "4:52"],
+			["assets/img/945cb857460591da1840c1097a7f8917.png", "Cool Breeze", "John Macentire", "sacared", "4:52"]],
 		"likes": 12,
 	},
 	{
 		"playlistID": 3,
 		"playlist_name": "Chill1",
 		"playlist_author": "Joe",
-		"playlistArt": "assets\img\playlist.png",
+		"playlistArt": "assets/img/playlist.png",
 		"likes": 13,
-		"songs": ["Cool Breeze", "ABCs"]
 	},
 	{
 		"playlistID": 1,
 		"playlist_name": "Chill2",
 		"playlist_author": "Joe",
-		"playlistArt": "assets\img\playlist.png",
+		"playlistArt": "assets/img/playlist.png",
 		"likes": 14,
 		"songs": ["Cool Breeze", "ABCs"]
 	},
@@ -27,7 +29,31 @@ let playlists = [
 		"playlistID": 1,
 		"playlist_name": "Chill3",
 		"playlist_author": "Joes",
-		"playlistArt": "assets\img\playlist.png",
+		"playlistArt": "assets/img/playlist.png",
+		"likes": 15,
+		"songs": ["Cool Breeze", "ABCs"]
+	},
+	{
+		"playlistID": 1,
+		"playlist_name": "Chill4",
+		"playlist_author": "Joes",
+		"playlistArt": "assets/img/playlist.png",
+		"likes": 15,
+		"songs": ["Cool Breeze", "ABCs"]
+	},
+	{
+		"playlistID": 1,
+		"playlist_name": "Chill5",
+		"playlist_author": "Joes",
+		"playlistArt": "assets/img/playlist.png",
+		"likes": 15,
+		"songs": ["Cool Breeze", "ABCs"]
+	},
+	{
+		"playlistID": 1,
+		"playlist_name": "Chill5",
+		"playlist_author": "Joes",
+		"playlistArt": "assets/img/playlist.png",
 		"likes": 15,
 		"songs": ["Cool Breeze", "ABCs"]
 	},
--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -3,9 +3,27 @@
 const modal = document.getElementById("playListModal");
 const span = document.getElementsByClassName("close")[0];
 
-function openModal(festival) {
-   
-   modal.style.display = "block";
+function openModal(playlist) {
+    console.log(playlist);
+   document.getElementById('playListName').innerText = playlist.playlist_name;
+   document.getElementById('playListImage').src = playlist.playlistArt;
+
+    for(const song in playlist.songs){
+        const newSong = document.createElement('div');
+        newSong.className = "modalSong"
+        newSong.innerHTML = `<div class="modalSongImgContainer">
+                            <img width="75x" height="75x" src="${playlist.songs[song][0]}" class="songImage">
+                        </div>
+                        <div class="songDetails">
+                            <h3>${playlist.songs[song][1]}</h3>
+                            <p>${playlist.songs[song][2]}</p>
+                            <p>${playlist.songs[song][3]}</p>
+                            <p>${playlist.songs[song][4]}</p>
+                        </div>`
+        document.getElementById('songs').appendChild(newSong);
+    }
+
+   modal.style.display = "flex";
 }
 
 span.onclick = function() {
@@ -20,14 +38,20 @@ window.onclick = function(event) {
 //Code for Playlist rendering
 
 const createPlaylist = (playlist) => {
-    console.log(playlist);
     const playlistElement = document.createElement('div');
     playlistElement.className = 'playList';
-    playlistElement.onclick = "openModal()"
+    playlistElement.style.width = "25%";
+    playlistElement.style.display = "flex";
+    playlistElement.style.flexDirection = "column";
+    playlistElement.style.alignItems = "center"
+    playlistElement.style.marginTop = "25px"
+    playlistElement.style.paddingTop = "10px"
+    playlistElement.style.border = "solid #bf5700 3px"
+
     playlistElement.innerHTML= `
-    <img width="100px" src="playlist.png">
+    <img style="max-width:75px; width:50%" src="${playlists[playlist].playlistArt}">
     <h4>${playlists[playlist].playlist_name}</h3>
-    <h6>${playlists[playlist].playlist_author}</h6>
+    <p> Created by ${playlists[playlist].playlist_author}</p>
     <div class="likes">
         <h6>&#x2665</h6>
         <h6>${playlists[playlist].likes}</h6>
@@ -40,6 +64,9 @@ const loadPlaylists = () =>{
     const container = document.querySelector('#playListGrid');
     for(const playlist in playlists){
         const el = createPlaylist(playlist);
+        el.addEventListener("click", function(event) {
+            openModal(playlists[playlist]);
+        })
         container.appendChild(el);
     }
 }
