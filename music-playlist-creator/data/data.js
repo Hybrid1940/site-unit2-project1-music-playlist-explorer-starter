03ad4c0bcedc211090678d2a773e14f480c76752
Mahesh Andres Bachu <maheshbachu@hotmail.com>
2025-06-05 11:18:52 -0700

    data update

--- music-playlist-creator/data/data.js
+++ music-playlist-creator/data/data.js
@@ -1,73 +1,122 @@
 let playlists = [
 	{
-		"playlistID": 1,
-		"playlist_name": "Chill",
-		"playlist_author": "Joe",
-		"playlistArt": "assets/img/945cb857460591da1840c1097a7f8917.png",
-		"songs": [
-			["assets/img/945cb857460591da1840c1097a7f8917.png", "Cool Breeze1", "John Macentire1", "sacared", "4:52"], 
-			["assets/img/945cb857460591da1840c1097a7f8917.png", "Cool Breeze2", "John Macentire2", "sacared", "4:52"],
-			["assets/img/945cb857460591da1840c1097a7f8917.png", "Cool Breeze3", "John Macentire3", "sacared", "4:52"],
-			["assets/img/945cb857460591da1840c1097a7f8917.png", "Cool Breeze4", "John Macentire3", "sacared", "4:52"],
-			["assets/img/945cb857460591da1840c1097a7f8917.png", "Cool Breeze5", "John Macentire3", "sacared", "4:52"]],
-		"likes": 13,
-		"likesColor": "black"
+    "playlistID": 1,
+    "playlist_name": "Summer Vibes",
+    "playlist_author": "Emily",
+    "playlistArt": "assets/img/945cb857460591da1840c1097a7f8917.png",
+    "songs": [
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Sunset Dream", "Lana Del Rey", "relaxing", "3:45"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Beachy Keen", "The Drums", "upbeat", "2:58"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Sunkissed", "The 1975", "indie", "4:10"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Tropical Oasis", "Kygo", "electronic", "4:25"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Seaside Escape", "The Lumineers", "folk", "3:20"]
+    ],
+    "likes": 27,
+    "likesColor": "black"
 	},
 	{
-		"playlistID": 3,
-		"playlist_name": "Chill1",
-		"playlist_author": "Joe",
-		"playlistArt": "assets/img/playlist.png",
-		"songs": [
-			["assets/img/945cb857460591da1840c1097a7f8917.png", "Cool Breeze", "John Macentire", "sacared", "4:52"], 
-			["assets/img/945cb857460591da1840c1097a7f8917.png", "Cool Breeze", "John Macentire", "sacared", "4:52"],
-			["assets/img/945cb857460591da1840c1097a7f8917.png", "Cool Breeze", "John Macentire", "sacared", "4:52"]],
-		"likes": 13,
-		"likesColor": "black"
+    "playlistID": 2,
+    "playlist_name": "Workout Motivation",
+    "playlist_author": "David",
+    "playlistArt": "assets/img/945cb857460591da1840c1097a7f8917.png",
+    "songs": [
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Pump Up the Jam", "Technotronic", "energetic", "3:35"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Eye of the Tiger", "Survivor", "classic rock", "3:59"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Uptown Funk", "Mark Ronson ft. Bruno Mars", "funky", "4:38"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Can't Stop", "Red Hot Chili Peppers", "rock", "4:29"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "We Will Rock You", "Queen", "anthemic", "2:28"]
+    ],
+    "likes": 42,
+    "likesColor": "black"
 	},
 	{
-		"playlistID": 1,
-		"playlist_name": "Chill2",
-		"playlist_author": "Joe",
-		"playlistArt": "assets/img/playlist.png",
-		"likes": 14,
-		"likesColor": "black",
-		"songs": ["Cool Breeze", "ABCs"]
+    "playlistID": 3,
+    "playlist_name": "Summer Vibes",
+    "playlist_author": "Emily",
+    "playlistArt": "assets/img/945cb857460591da1840c1097a7f8917.png",
+    "songs": [
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Sunset Dream", "Lana Del Rey", "relaxing", "3:45"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Beachy Keen", "The Drums", "upbeat", "2:58"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Sunkissed", "The 1975", "indie", "4:10"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Tropical Oasis", "Kygo", "electronic", "4:25"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Seaside Escape", "The Lumineers", "folk", "3:20"]
+    ],
+    "likes": 27,
+    "likesColor": "black"
 	},
 	{
-		"playlistID": 1,
-		"playlist_name": "Chill3",
-		"playlist_author": "Joes",
-		"playlistArt": "assets/img/playlist.png",
-		"likes": 15,
-		"likesColor": "black",
-		"songs": ["Cool Breeze", "ABCs"]
+    "playlistID": 4,
+    "playlist_name": "Workout Motivation",
+    "playlist_author": "David",
+    "playlistArt": "assets/img/945cb857460591da1840c1097a7f8917.png",
+    "songs": [
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Pump Up the Jam", "Technotronic", "energetic", "3:35"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Eye of the Tiger", "Survivor", "classic rock", "3:59"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Uptown Funk", "Mark Ronson ft. Bruno Mars", "funky", "4:38"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Can't Stop", "Red Hot Chili Peppers", "rock", "4:29"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "We Will Rock You", "Queen", "anthemic", "2:28"]
+    ],
+    "likes": 42,
+    "likesColor": "black"
 	},
 	{
-		"playlistID": 1,
-		"playlist_name": "Chill4",
-		"playlist_author": "Joes",
-		"playlistArt": "assets/img/playlist.png",
-		"likes": 15,
-		"likesColor": "black",
-		"songs": ["Cool Breeze", "ABCs"]
+    "playlistID": 5,
+    "playlist_name": "Summer Vibes",
+    "playlist_author": "Emily",
+    "playlistArt": "assets/img/945cb857460591da1840c1097a7f8917.png",
+    "songs": [
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Sunset Dream", "Lana Del Rey", "relaxing", "3:45"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Beachy Keen", "The Drums", "upbeat", "2:58"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Sunkissed", "The 1975", "indie", "4:10"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Tropical Oasis", "Kygo", "electronic", "4:25"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Seaside Escape", "The Lumineers", "folk", "3:20"]
+    ],
+    "likes": 27,
+    "likesColor": "black"
 	},
 	{
-		"playlistID": 1,
-		"playlist_name": "Chill5",
-		"playlist_author": "Joes",
-		"playlistArt": "assets/img/playlist.png",
-		"likes": 15,
-		"likesColor": "black",
-		"songs": ["Cool Breeze", "ABCs"]
+    "playlistID": 6,
+    "playlist_name": "Workout Motivation",
+    "playlist_author": "David",
+    "playlistArt": "assets/img/945cb857460591da1840c1097a7f8917.png",
+    "songs": [
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Pump Up the Jam", "Technotronic", "energetic", "3:35"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Eye of the Tiger", "Survivor", "classic rock", "3:59"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Uptown Funk", "Mark Ronson ft. Bruno Mars", "funky", "4:38"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Can't Stop", "Red Hot Chili Peppers", "rock", "4:29"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "We Will Rock You", "Queen", "anthemic", "2:28"]
+    ],
+    "likes": 42,
+    "likesColor": "black"
 	},
 	{
-		"playlistID": 1,
-		"playlist_name": "Chill5",
-		"playlist_author": "Joes",
-		"playlistArt": "assets/img/playlist.png",
-		"likes": 15,
-		"likesColor": "black",
-		"songs": ["Cool Breeze", "ABCs"]
+    "playlistID": 7,
+    "playlist_name": "Summer Vibes",
+    "playlist_author": "Emily",
+    "playlistArt": "assets/img/945cb857460591da1840c1097a7f8917.png",
+    "songs": [
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Sunset Dream", "Lana Del Rey", "relaxing", "3:45"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Beachy Keen", "The Drums", "upbeat", "2:58"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Sunkissed", "The 1975", "indie", "4:10"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Tropical Oasis", "Kygo", "electronic", "4:25"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Seaside Escape", "The Lumineers", "folk", "3:20"]
+    ],
+    "likes": 27,
+    "likesColor": "black"
+	},
+	{
+    "playlistID": 8,
+    "playlist_name": "Workout Motivation",
+    "playlist_author": "David",
+    "playlistArt": "assets/img/945cb857460591da1840c1097a7f8917.png",
+    "songs": [
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Pump Up the Jam", "Technotronic", "energetic", "3:35"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Eye of the Tiger", "Survivor", "classic rock", "3:59"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Uptown Funk", "Mark Ronson ft. Bruno Mars", "funky", "4:38"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "Can't Stop", "Red Hot Chili Peppers", "rock", "4:29"],
+        ["assets/img/945cb857460591da1840c1097a7f8917.png", "We Will Rock You", "Queen", "anthemic", "2:28"]
+    ],
+    "likes": 42,
+    "likesColor": "black"
 	},
 ]
